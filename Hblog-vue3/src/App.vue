9a7529953081ee398b6b952c025ee41570b970d6
Rita he <1762704748@qq.com>
2026-07-19 13:47:01 +0800

    feat: 更新 Hblog Vue3 项目，添加 Vite 配置，重构路由和组件结构，支持 Vue 文件类型声明

--- Hblog-vue3/src/App.vue
+++ Hblog-vue3/src/App.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
-  <RouterView />
+   <router-view></router-view>
 </template>
 
-<style scoped></style>
+<script setup>
+ 
+</script>
+
+<style scoped>
+
+</style>
---
+++ Hblog-vue3/src/pages/frontend/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>首页</h1>
+</template>
+
+<style scoped>
+h1 {
+  margin: 1rem;
+  color: #1a1a1a;
+}
+</style>
